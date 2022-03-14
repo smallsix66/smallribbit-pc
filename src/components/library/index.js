9e843f317a750fa6ca9741aab774aa6e87fe0036
liuhouhou <1198369824@qq.com>
2022-03-14 17:01:15 +0800

    顶级类目（render动态创建面包屑组件（解决多余箭头）+require.context(dir,deep,matching)批量注册组件+监听路由地址id重新渲染组件+面包屑切换动画（key、mode解决触发动画和东华顺序）

--- src/components/library/index.js
+++ src/components/library/index.js
@@ -6,16 +6,36 @@
 import XtxSkeleton from './xtx-skeleton.vue'
 import XtxCarousel from './xtx-carousel.vue'
 import XtxMore from './xtx-more.vue'
+import XtxBread from './xtx-bread.vue'
+import XtxBreadItem from './xtx-bread-item.vue'
 
 import defaultImg from '@/assets/images/200.png'
 
+
+// 导入library文件夹下的所有组件
+// 批量导入需要使用一个函数 require.context(dir,deep,matching)
+// 参数：1. 目录  2. 是否加载子目录  3. 加载的正则匹配
+const importFn = require.context('./',false,/\.vue$/)
+// console.dir(importFn.keys()) //文件名称数组
+
 export default {
-  install (app) {
+  install(app) {
     // 在app上进行扩展，app提供 component directive 函数
     // 如果要挂载原型 app.config.globalProperties 方式
-    app.component(XtxSkeleton.name, XtxSkeleton)
-    app.component(XtxCarousel.name, XtxCarousel)
-    app.component(XtxMore.name, XtxMore)
+    // app.component(XtxSkeleton.name, XtxSkeleton)
+    // app.component(XtxCarousel.name, XtxCarousel)
+    // app.component(XtxMore.name, XtxMore)
+    // app.component(XtxBread.name, XtxBread)
+    // app.component(XtxBreadItem.name, XtxBread)
+
+    // 批量注册全局组件
+    importFn.keys().forEach(key  => {
+      // 导入组件
+      const component = importFn(key).default
+      // 注册组件
+      app.component(component.name, component)
+    });
+
     defineDirective(app)
   }
 }
@@ -24,13 +44,13 @@ export default {
 const defineDirective = (app) => {
   // 图片懒加载指令
   app.directive('lazyload', {
-    mounted (el, binding) {
+    mounted(el, binding) {
       const observer = new IntersectionObserver(([{ isIntersecting }]) => {
         if (isIntersecting) {
           // console.log('进入可视区',el);
           observer.unobserve(el)
           el.onerror = () => {
-              el.src = defaultImg
+            el.src = defaultImg
           }
           el.src = binding.value
         }
---
+++ src/components/library/xtx-bread.vue
@@ -0,0 +1,39 @@
+<script>
+import {h} from 'vue'
+export default {
+  name: "XtxBread",
+ render(){
+   const items = this.$slots.default()
+   const dymanicItems = []
+   items.forEach((item,i)=>{
+     dymanicItems.push(item)
+     if(i<(items.length-1)){
+       dymanicItems.push(h('i',{class:'iconfont icon-arrow-right'}))
+     }
+   })
+   return h('div',{class:'xtx-bread'},dymanicItems)
+ }
+};
+</script>
+
+<style scoped lang='less'>
+.xtx-bread {
+  display: flex;
+  padding: 25px 10px;
+  &-item {
+    a {
+      color: #666;
+      transition: all 0.4s;
+      &:hover {
+        color: @xtxColor;
+      }
+    }
+  }
+  i {
+    font-size: 12px;
+    margin-left: 5px;
+    margin-right: 5px;
+    line-height: 22px;
+  }
+}
+</style>
