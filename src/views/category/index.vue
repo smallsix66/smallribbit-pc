37bacd136a174e054dafe71e2dce16f535a1baa3
liuhouhou <1198369824@qq.com>
2022-03-16 17:15:59 +0800

    二级类目（跳转二级避免重复查询+面包屑+筛选区+复选框封装（双向绑定）+排序组件+数据加载组件+筛选（查询并保留原数据））

--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -80,7 +80,8 @@ export default {
     watch(
       () => route.params.id,
       (newVal) => {
-        newVal && getSubList();
+        // newVal && getSubList();
+        if (newVal && `/category/${newVal}` === route.path) getSubList();
       },
       { immediate: true }
     );
--- src/views/category/sub.vue
+++ src/views/category/sub.vue
@@ -1,12 +1,126 @@
 <template>
-  <div>SubCategory</div>
+  <div class="sub-category">
+    <div class="container">
+      <!-- 面包屑 -->
+      <SubBread />
+      <!-- 筛选区 -->
+      <SubFilter @filter-change="changeFilter" />
+      <div class="goods-list">
+        <!-- 排序 -->
+        <SubSort @sort-change="changeSort" />
+        <!-- 列表 -->
+        <ul>
+          <li v-for="(goods, i) in goodsList" :key="i">
+            <GoodsItem :goods="goods" />
+          </li>
+        </ul>
+        <!-- 加载 -->
+        <XtxInfiniteLoading
+          :loading="loading"
+          :finished="finished"
+          @infinite="getData"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import SubBread from "./components/sub-bread";
+import SubFilter from "./components/sub-filter.vue";
+import SubSort from "./components/sub-sort.vue";
+import GoodsItem from "./components/goods-item";
+import { ref, watch } from "vue";
+import { findSubCategoryGoods } from "@/api/category";
+import { useRoute } from "vue-router";
 export default {
-  name:'SubCategory'
+  name: "SubCategory",
+  components: { SubBread, SubFilter, SubSort, GoodsItem },
+  setup() {
+    // 1. 基础布局
+    // 2. 无限加载组件
+    // 3. 动态加载数据且渲染
+    // 4. 任何筛选条件变化需要更新列表
+    const route = useRoute();
+
+    const loading = ref(false);
+    const finished = ref(false);
+    const goodsList = ref([]);
+    // 查询参数
+    let reqParams = {
+      page: 1,
+      pageSize: 20,
+    };
+    // 获取数据函数
+    const getData = () => {
+      loading.value = true;
+      reqParams.categoryId = route.params.id;
+      findSubCategoryGoods(reqParams).then(({ result }) => {
+        if (result.items.length) {
+          goodsList.value.push(...result.items);
+          reqParams.page++;
+        } else {
+          // 加载完毕
+          finished.value = true;
+        }
+        // 请求结束
+        loading.value = false;
+      });
+    };
+
+    // 切换二级分类重新加载
+    watch(
+      () => route.params.id,
+      (newVal) => {
+        if (newVal && route.path === "/category/sub/" + newVal) {
+          goodsList.value = [];
+          reqParams = {
+            page: 1,
+            pageSize: 20,
+          };
+          finished.value = false;
+        }
+      }
+    );
+
+    // 监听筛选区改变
+    const changeFilter = (filterParams) => {
+      console.log('brand',filterParams);
+      reqParams = { ...reqParams, ...filterParams };
+      reqParams.page = 1;
+      goodsList.value = [];
+      finished.value = false;
+    };
+    // 监听排序改变
+    const changeSort = (sortParams) => {
+      console.log('sort',sortParams);
+      reqParams = { ...reqParams, ...sortParams };
+      reqParams.page = 1;
+      goodsList.value = [];
+      finished.value = false;
+    };
+
+    return { loading, finished, goodsList, getData, changeFilter, changeSort };
+  },
 };
 </script>
 
-<style scoped lang="less">
+<style scoped lang='less'>
+.goods-list {
+  background: #fff;
+  padding: 0 25px;
+  margin-top: 25px;
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 0 5px;
+    li {
+      margin-right: 20px;
+      margin-bottom: 20px;
+      &:nth-child(5n) {
+        margin-right: 0;
+      }
+    }
+  }
+}
 </style>
